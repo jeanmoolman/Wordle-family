d1ee82e69f1b1293f3c6022317302f34addf59df
jeanmoolman <jeanmoolman@gmail.com>
2025-11-15 14:12:53 +0200

    Update SECRET_WORD to 'tough'

--- config.js
+++ config.js
@@ -4,4 +4,5 @@
  * Change the SECRET_WORD here to update the daily word.
  * It must be a 5-letter word found in the dictionary array in index.html.
  */
-const SECRET_WORD = "CREAM";
+
+const SECRET_WORD = "tough";

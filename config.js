b4c0ff056f51d5d4955b5beadcd7b02d86be069a
jeanmoolman <jeanmoolman@gmail.com>
2025-11-15 11:55:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+
+/**
+ * Change the SECRET_WORD here to update the daily word.
+ * It must be a 5-letter word found in the dictionary array in index.html.
+ */
+const SECRET_WORD = "CREAM";
